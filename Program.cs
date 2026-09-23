b1db10fca54ef0a598395a448ffef4aa98081563
Elias Jaktlund <jaktlundelias@gmail.com>
2026-09-23 10:35:01 +0200

    Add personalized welcome message

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Vänligen ange ditt namn:");
+string name = Console.ReadLine();
+if (string .IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Du måste ange ett namn");
+}
+else
+{
+    Console.WriteLine($"Välkommen {name}");
+}
